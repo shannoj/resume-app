45c2b09bd1202eab015272fe7c02e9422d835aff
shannoj <69207315+shannoj@users.noreply.github.com>
2023-11-09 16:42:22 +0100

    added icons from fontawesome and made the dropdown boxes look better

--- resume-app/src/App.tsx
+++ resume-app/src/App.tsx
@@ -1,13 +1,30 @@
 import "./App.css";
 import Dropdown from "./components/Dropdown";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUserGraduate,
+  faUser,
+  faBriefcase,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
   return (
     <>
       <div className="information-container mx-5 mt-10 mb-10 rounded-md shadow-md">
-        <Dropdown image="" title="Background Information"></Dropdown>
-        <Dropdown image="" title="Education"></Dropdown>
-        <Dropdown image="" title="Experience"></Dropdown>
+        <Dropdown title="Background Information">
+          {
+            <FontAwesomeIcon
+              className="flex flex-col justify-center align-center"
+              icon={faUser}
+            ></FontAwesomeIcon>
+          }
+        </Dropdown>
+        <Dropdown title="Education">
+          {<FontAwesomeIcon icon={faUserGraduate}></FontAwesomeIcon>}
+        </Dropdown>
+        <Dropdown title="Experience">
+          {<FontAwesomeIcon icon={faBriefcase}></FontAwesomeIcon>}
+        </Dropdown>
       </div>
       <div className="resume-container"></div>
     </>
--- resume-app/src/components/Dropdown.tsx
+++ resume-app/src/components/Dropdown.tsx
@@ -1,19 +1,27 @@
+import { ReactNode } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+
 interface Props {
   title: string;
-  image: string;
+  children: ReactNode;
 }
 
-function Dropdown({ title, image }: Props) {
+function Dropdown({ title, children }: Props) {
   return (
     <>
-      <div className="flex flex-row justify-center align-center w-4/5 h-12 bg-neutral-50 rounded-md mt-6 shadow-md">
-        <div className="box flex flex-row justify-start align-center">
-          <img></img>
-          <p className="flex flex-column justify-center align-center font-serif">
+      <div className="flex flex-row justify-between align-center w-4/5 h-12 bg-neutral-50 rounded-md mt-6 shadow-md hover:shadow-lg">
+        <div className="box flex flex-row justify-start align-center w-3/5 h-full">
+          <div className="flex flex-col justify-center align-center h-full ml-2">
+            {children}
+          </div>
+          <p className="flex flex-col justify-center align-center font-serif h-full ml-2">
             {title}
           </p>
         </div>
-        <img src={image} className="drop-down-arrow"></img>
+        <div className="flex h-full flex-col justify-center align-center w-1/5">
+          <FontAwesomeIcon icon={faAngleDown}></FontAwesomeIcon>
+        </div>
       </div>
     </>
   );
