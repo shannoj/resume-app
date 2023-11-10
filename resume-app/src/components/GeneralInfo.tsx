077922d67790c81033f032dfc588302ff4eb73d9
shannoj <69207315+shannoj@users.noreply.github.com>
2023-11-10 10:18:41 +0100

    added last name

--- resume-app/src/components/GeneralInfo.tsx
+++ resume-app/src/components/GeneralInfo.tsx
@@ -3,7 +3,7 @@ function General() {
     <>
       <form className="w-full max-w-sm">
         <div className="md:flex md:items-center mb-6">
-          <div className="md:w-1/3">
+          <div className="md:w-1/3 mt-2 ml-2">
             <label
               className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
               htmlFor="inline-first-name"
@@ -11,12 +11,30 @@ function General() {
               First Name
             </label>
           </div>
-          <div className="md:w-2/3">
+          <div className="md:w-2/3 mt-2 mr-2">
             <input
               className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
               id="inline-first-name"
               type="text"
               value="Jane"
+            ></input>
+          </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3 mt-2 ml-2">
+            <label
+              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              htmlFor="inline-first-name"
+            >
+              Last Name
+            </label>
+          </div>
+          <div className="md:w-2/3 mt-2 mr-2">
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="inline-first-name"
+              type="text"
+              value="Doe"
             ></input>
           </div>
         </div>
