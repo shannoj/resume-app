b92dd67eb62923f289582b0438b94cd15a3d8ccb
shannoj <69207315+shannoj@users.noreply.github.com>
2023-11-26 14:06:47 +0100

    fixed more layout issues

--- resume-app/src/components/EducationInfo.tsx
+++ resume-app/src/components/EducationInfo.tsx
@@ -179,7 +179,7 @@ function EducationInfo({
                 {formSubmitted ? (
                   editMode ? (
                     <button
-                      className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                      className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
                       type="submit"
                       onClick={handleSaveChangesClick}
                     >
@@ -187,7 +187,7 @@ function EducationInfo({
                     </button>
                   ) : (
                     <button
-                      className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                      className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
                       type="button"
                       onClick={handleEditClick}
                     >
@@ -196,7 +196,7 @@ function EducationInfo({
                   )
                 ) : (
                   <button
-                    className="shadow bg-blue-500 hover:bg-blue-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+                    className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
                     type="submit"
                     id={`education-form-${index}`}
                   >
--- resume-app/src/components/WorkResumeBlock.tsx
+++ resume-app/src/components/WorkResumeBlock.tsx
@@ -27,7 +27,6 @@ function WorkResumeBlock({ data }: Work) {
           </div>
         ))}
       </div>
-      ;
     </>
   );
 }
